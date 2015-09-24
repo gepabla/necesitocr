e204ea2e56f381e3c8bae14327317010a9e95d90
Johan Chaves <jchavesarias@gmail.com>
2015-09-23 23:29:27 -0600

    results UI added.

---
+++ src/main/webapp/resources/js/directives.js
@@ -0,0 +1,24 @@
+angular.module('necesitocr.directives', [])
+.directive('searchContainer', function($rootScope) {
+    return {
+        restrict: 'A',
+        scope : {
+        	searchContainerModel : "=searchContainer"
+        },
+        link: function(scope, element, attrs) {  
+        	console.log("attrs" , scope);
+        	scope.$watch("searchContainerModel",function(){
+        		if(!scope.searchContainerModel.isBigContainer){
+        			var windowHeight = $(window).height();            
+                    var elem = $(element);
+                    elem.animate({height : 300});
+                    var eva = elem.find(".eva-container img");
+                    eva.animate({height : 200,width : 200});
+        		}
+        	},true);
+            var windowHeight = $(window).height();            
+            var elem = $(element);
+            elem.height(windowHeight);
+        }
+    };
+})
